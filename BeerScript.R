25ebcf6e8075cdcd66e916e343d1c8f4a36c7348
louiserply <rapilly.louise@gmail.com>
2017-01-27 12:14:17 +0100

    question2(v3)

--- BeerScript.R
+++ BeerScript.R
@@ -34,9 +34,32 @@ d = randomSample[, c(5,6,9,10)]
 #k-means avec 5 clusters
 beerCluster <- kmeans(randomSample[, c(5,6,9,10)], 5)
 
+#show the centers
+beerCluster$centers
+
+#show the clusters
+beerCluster$cluster
+
 plotcluster(d, beerCluster$cluster)
+#show the centers
+#points(beerCluster$centers, pch=2,col='red')
+
 with(beerdata, pairs(d, col=c(5,6,9,10)[beerCluster$cluster])) 
 
-clPairs(d)
-BIC = mclustBIC(d)
-plot(BIC)
+#http://eric.univ-lyon2.fr/~ricco/cours/didacticiels/R/cah_kmeans_avec_r.pdf
+
+#methode des centres mobiles : 2 methodes
+
+#1ere methode : évaluer la proportion d'inertie expliquée
+inertie.expl <- rep(0,times=10)
+for (k in 2:10){
+    clus <- kmeans(d.cr,centers=k,nstart=5)
+    inertie.expl[k] <- clus$betweenss/clus$totss }
+plot(1:10,inertie.expl,type="b",xlab="Nb. de groupes",ylab="% inertie expliquée")
+
+#2eme methode : largeur moyenne de silhouette - utilisation du package fpc
+sol.kmeans <- kmeansruns(d.cr,krange=2:10,criterion="ch")
+plot(1:10,sol.kmeans$crit,type="b",xlab="Nb. de groupes",ylab="Silhouette")
+
+#Les deux methodes indiquent que le nombre optimal de clusters est 2
+
