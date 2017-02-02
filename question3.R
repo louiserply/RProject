81746511b164513b098836a0a38d08f0e14b7627
Matthiosso <clement_m@esme.fr>
2017-02-02 11:58:15 +0100

    Worked on question 3

--- question3.R
+++ question3.R
@@ -1,3 +1,17 @@
 ## Question 3
 
 library(recommenderlab)
+library(data.table)
+
+beerdata = data.table::fread("beer_reviews.csv")
+
+# Loading to pre-computed affinity data	
+affinity.data<-beerdata[,c(4,11)]
+affinity.matrix<- as(affinity.data,"realRatingMatrix")
+
+# Creation of the model - U(ser) B(ased) C(ollaborative) F(iltering)
+Rec.model<-Recommender(affinity.matrix, method = "UBCF")
+
+recommended.items.1 <- predict(Rec.model, affinity.matrix["1",], n=5)
+
+as(recommended.items.1, "list")
