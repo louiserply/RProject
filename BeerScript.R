0f1e04922d6fed50d83dee334651eb7d7376d6b0
louiserply <rapilly.louise@gmail.com>
2017-02-10 14:20:59 +0100

    update final

--- BeerScript.R
+++ BeerScript.R
@@ -14,23 +14,26 @@ beerdata = data.table::fread("beer_reviews.csv")
 ### Top10 for Aroma
 aggregAroma = aggregate(beerdata$review_aroma, by=list(beerdata$beer_name), FUN=mean)
 top10Aroma = head(aggregAroma[ order(-aggregAroma[,2], aggregAroma[,1]), ], 10)
+top10Aroma
 
 ### Top 10 for Palate:
 aggregPalate = aggregate(beerdata$review_palate, by=list(beerdata$beer_name), FUN=mean)
 top10Palate = head(aggregPalate[ order(-aggregPalate[,2], aggregPalate[,1]), ], 10)
+top10Palate
 
 ### Top 10 for Appearance:
 aggregAppearance = aggregate(beerdata$review_appearance, by=list(beerdata$beer_name), FUN=mean)
 top10Appearance = head(aggregAppearance[ order(-aggregAppearance[,2], aggregAppearance[,1]), ], 10)
+top10Appearance
 
 ### Top 10 for Taste:
 aggregTaste = aggregate(beerdata$review_taste, by=list(beerdata$beer_name), FUN=mean)
 top10Taste = head(aggregTaste[ order(-aggregTaste[,2], aggregTaste[,1]), ], 10) 
-
+top10Taste
 
 ## Question 2
 
-#sous-échantillon aléatoire de 2000 observations
+#random sample of 2000 items
 randomSample = beerdata[sample(nrow(beerdata), 2000), ]
 
 d = randomSample[, c(5,6,9,10)]
@@ -38,8 +41,8 @@ d = randomSample[, c(5,6,9,10)]
 #jitter
 d.jitter = data.frame(lapply(d, jitter))
 
-#k-means avec 5 clusters
-beerCluster <- kmeans(d, 2)
+#k-means with 3 clusters
+beerCluster <- kmeans(d, 3)
 
 #show the centers
 beerCluster$centers
@@ -56,9 +59,9 @@ splom(d.jitter, col=c(5,6,9,10)[beerCluster$cluster])
 
 
 
-#methode des centres mobiles : 2 methodes
+#K-means clustering : 2 ways of doing it
 
-#1ere methode : évaluer la proportion d'inertie expliquée
+#First way : evaluate the inertie
 d.cr <- scale(d,center=T,scale=T)
 
 inertie.expl <- rep(0,times=10)
@@ -67,51 +70,69 @@ for (k in 2:10){
     inertie.expl[k] <- clus$betweenss/clus$totss }
 plot(1:10,inertie.expl,type="b",xlab="Nb. de groupes",ylab="% inertie expliquée")
 
-#2eme methode : largeur moyenne de silhouette - utilisation du package fpc
+#Second way : usage of fpc package
 sol.kmeans <- kmeansruns(d.cr,krange=2:10,criterion="ch")
 plot(1:10,sol.kmeans$crit,type="b",xlab="Nb. de groupes",ylab="Silhouette")
 
-#Les deux methodes indiquent que le nombre optimal de clusters est 2
+#These two experiments both show that the optimal number of class is 2
 
 
-#dendrogramme
+#dendrogram
 
-#d.cr <- scale(d,center=T,scale=T)
-#dendro.d <- dist(d.cr)
-#cah.ward <- hclust(dendro.d,method="ward.D2")
-#plot(cah.ward)
+d.cr <- scale(d,center=T,scale=T)
+dendro.d <- dist(d.cr)
+cah.ward <- hclust(dendro.d,method="ward.D2")
+plot(cah.ward)
+groupes.cah <- cutree(cah.ward,k=3)
 
-#rect.hclust(cah.ward,k=3)
-#groupes.cah <- cutree(cah.ward,k=3)
-
-#fonction de calcul des stats
+#Stat calcul function
 stat.comp <- function(x,y){
-  #nombre de groupes
+  #number of group
   K <- length(unique(y))
-  #nb. d'observations
+  #number of observations
   n <- length(x)
-  #moyenne globale
+  #global mean
   m <- mean(x)
-  #variabilité totale
+  #total variablility
   TSS <- sum((x-m)^2)
-  #effectifs conditionnels
+  #conditional effective
   nk <- table(y)
-  #moyennes conditionnelles
+  #conditional mean
   mk <- tapply(x,y,mean)
-  #variabilité expliqué
+  #explained variability
   BSS <- sum(nk * (mk - m)^2)
-  #moyennes + prop. variance expliquée
+  #means plus explained variance
   result <- c(mk,100.0*BSS/TSS)
-  #nommer les élements du vecteur
+  #we name the elements of the vector
   names(result) <- c(paste("G",1:K),"% epl.")
-  #renvoyer le vecteur résultat
+  #we return the vector
   return(result) }
 
-#appliquer stat.comp aux variables de la base originelle d
-#et non pas aux variables centrées et réduites 
+#we apply stat.comp to the variable d and not to the standardardized normal distribution
 print(sapply(d,stat.comp,y=groupes.cah))
 
-#La définition des groupes est avant tout dominée par review_aroma
+#Groups are mostly dominated by review_aroma, review_palate and review_appearance.
+
+
+## Question 3
+
+library(recommenderlab)
+library(data.table)
+
+beerdata = data.table::fread("beer_reviews.csv")
+
+# Loading to pre-computed affinity data	
+affinity.data<-beerdata[,c(4,11)]
+affinity.matrix<- as(affinity.data,"realRatingMatrix")
+
+# Creation of the model - U(ser) B(ased) C(ollaborative) F(iltering)
+Rec.model<-Recommender(affinity.matrix, method = "UBCF")
+
+recommended.items.1 <- predict(Rec.model, affinity.matrix["1",], n=10)
+
+as(recommended.items.1, "list")
+
+
 
 
 
